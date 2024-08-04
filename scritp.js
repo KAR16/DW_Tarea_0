fbbf00edb355b559d92d47e36e577f8c653418f0
KAR16 <kherrera16@gmail.com>
2024-08-03 21:39:52 -0600

    Se añadió la funcionalidad de la alerta para mostrar los datos del formulario

---
+++ scritp.js
@@ -0,0 +1,21 @@
+// let usuario = document.getElementById('usuario').value;
+console.log("Hola Mundo");
+
+
+
+function sendInfo(){
+    let nombres = document.getElementById('names').value;
+    let apellidos = document.getElementById('lastnames').value;
+    let telefono = document.getElementById('phone').value;
+    let fecha = document.getElementById('date').value;
+    let mensaje = document.getElementById('message').value;
+
+    alert(`${nombres} \n ${apellidos} \n ${telefono} \n ${fecha} \n ${mensaje}`);
+}
+
+
+
+
+// alert("Hola Mundo");
+//el formulario se envia
+
